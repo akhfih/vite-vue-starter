6c619b584d3bc506055b013bf91b133553c8e7e1
akhfih <akhfih.dev@gmail.com>
2023-05-30 20:21:31 +0700

    feat: configure sass

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,11 @@ import eslintPlugin from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), eslintPlugin()],
+  css: {
+    preprocessorOptions: {
+      scss: { additionalData: `@import "src/assets/scss/main.scss";` },
+    },
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
